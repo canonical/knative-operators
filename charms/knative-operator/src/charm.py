d36c1edc01a981258d7c7a118601b3ee0bae0036
Daniela Plascencia <daniela.plascencia@canonical.com>
2022-05-26 14:50:05 -0500

    knative-operator/src/charm.py: small refactor
    We now have a `_main` method for handling the main changes
    in the charm: applying k8s resources and configuring pebble layers.
    This commit also adds exception handling and logging for when pebble
    throws an APIError.
    Finally, restarting the underlying service should be done only if
    changes were made, thus moving the restart call inside the statement
    that checks for changes.

--- charms/knative-operator/src/charm.py
+++ charms/knative-operator/src/charm.py
@@ -7,10 +7,12 @@ import traceback
 
 from ops.main import main
 from ops.pebble import Layer
+from ops.pebble import APIError as pebble_APIError
 from ops.charm import CharmBase
 from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
 from jinja2 import Environment, FileSystemLoader
-from lightkube import ApiError, Client, codecs
+from lightkube import Client, codecs
+from lightkube import ApiError as lightkube_ApiError
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +23,6 @@ class KnativeOperatorCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
 
-        self.logger = logging.getLogger(__name__)
         self.interfaces = ""
 
         self._name = self.model.app.name
@@ -39,9 +40,8 @@ class KnativeOperatorCharm(CharmBase):
         self._context = {"namespace": self._namespace, "name": self._name}
 
         self.lightkube_client = Client(namespace=self._namespace, field_manager="lightkube")
-        self.framework.observe(self.on.install, self._on_install)
-        self.framework.observe(self.on.start, self._on_start)
-        self.framework.observe(self.on.config_changed, self._on_install)
+        for event in [self.on.install, self.on.config_changed]:
+            self.framework.observe(event, self._main)
         self.framework.observe(
             self.on.knative_operator_pebble_ready,
             self._on_knative_operator_pebble_ready,
@@ -72,10 +72,11 @@ class KnativeOperatorCharm(CharmBase):
         }
         return Layer(layer_config)
 
-    def _update_layer(self) -> None:
+    def _update_layer(self, event) -> None:
         """Updates the Pebble configuration layer if changed."""
         if not self._container.can_connect():
             self.unit.status = MaintenanceStatus("Waiting for pod startup to complete")
+            event.defer()
             return
 
         # Get current config
@@ -85,7 +86,7 @@ class KnativeOperatorCharm(CharmBase):
         if current_layer.services != new_layer.services:
             self._container.add_layer(self._operator_service, new_layer, combine=True)
             logging.info("Pebble plan updated with new configuration")
-        self._container.restart(self._operator_service)
+            self._container.restart(self._operator_service)
 
     def _apply_all_resources(self) -> None:
         """Helper method to create Kubernetes resources."""
@@ -94,36 +95,39 @@ class KnativeOperatorCharm(CharmBase):
             for obj in codecs.load_all_yaml(manifest):
                 self.lightkube_client.apply(obj)
 
-    def _on_install(self, event):
-        """Event handler for InstallEvent."""
-
-        # Update Pebble configuration layer if it has changed
-        self._update_layer()
-
-        # Patch/create Kubernetes resources
+    def _main(self, event):
+        """Event handler for changing Pebble configuration and applying k8s resources."""
         try:
+            # Update Pebble configuration layer if it has changed
+            self.unit.status = MaintenanceStatus("Configuring Pebble layer")
+            self._update_layer(event)
             self.unit.status = MaintenanceStatus("Applying resources")
+            # Apply Kubernetes resources
             self._apply_all_resources()
-        except ApiError as e:
-            logging.error(traceback.format_exc())
-            self.unit.status = BlockedStatus(
-                f"Applying resources failed with code {str(e.status.code)}."
-            )
-            if e.status.code == 403:
-                logging.error(
-                    "Received Forbidden (403) error when creating auth resources."
-                    "This may be due to the charm lacking permissions to create"
-                    "cluster-scoped resources."
-                    "Charm must be deployed with --trust"
+        except (pebble_APIError, lightkube_ApiError) as e:
+            if isinstance(e, pebble_APIError):
+                logging.error(traceback.format_exc())
+                self.unit.status = BlockedStatus("Error updating Pebble configuration layer")
+            elif isinstance(e, lightkube_ApiError):
+                logging.error(traceback.format_exc())
+                self.unit.status = BlockedStatus(
+                    f"Applying resources failed with code {str(e.status.code)}."
                 )
-                event.defer()
-                return
+                if e.status.code == 403:
+                    logging.error(
+                        "Received Forbidden (403) error when creating auth resources."
+                        "This may be due to the charm lacking permissions to create"
+                        "cluster-scoped resources."
+                        "Charm must be deployed with --trust"
+                    )
+                    event.defer()
+                    return
         else:
             self.unit.status = ActiveStatus()
 
-    def _on_knative_operator_pebble_ready(self, _):
+    def _on_knative_operator_pebble_ready(self, event):
         """Event handler for on PebbleReadyEvent"""
-        self._update_layer()
+        self._update_layer(event)
 
 
 if __name__ == "__main__":
