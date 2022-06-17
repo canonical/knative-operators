e8391b718182332074d393b20287f4644854ef5b
Daniela Plascencia <daniela.plascencia@canonical.com>
2022-06-17 01:10:44 -0500

    knative-eventing: adds unit tests

--- charms/knative-eventing/tests/unit/test_charm.py
+++ charms/knative-eventing/tests/unit/test_charm.py
@@ -1,67 +1,69 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
-#
-# Learn more about testing at: https://juju.is/docs/sdk/testing
 
-import unittest
-from unittest.mock import Mock
-
-from ops.model import ActiveStatus
-from ops.testing import Harness
-
-from charm import OperatorTemplateCharm
+import pytest
+from k8s_resource_handler.exceptions import ReconcileError
+from k8s_resource_handler.lightkube.mocking import FakeApiError
+from ops.model import ActiveStatus, BlockedStatus
 
 
-class TestCharm(unittest.TestCase):
-    def setUp(self):
-        self.harness = Harness(OperatorTemplateCharm)
-        self.addCleanup(self.harness.cleanup)
-        self.harness.begin()
+def test_events(harness, mocked_lightkube_client, mocker):
+    # Test install and config_changed event handlers are called
+    harness.begin()
+    install = mocker.patch("charm.KnativeEventingCharm._on_install")
+    config_changed = mocker.patch("charm.KnativeEventingCharm._on_config_changed")
+    remove = mocker.patch("charm.KnativeEventingCharm._on_remove")
 
-    def test_config_changed(self):
-        self.assertEqual(list(self.harness.charm._stored.things), [])
-        self.harness.update_config({"thing": "foo"})
-        self.assertEqual(list(self.harness.charm._stored.things), ["foo"])
+    harness.charm.on.install.emit()
+    install.assert_called_once()
+    install.reset_mock()
 
-    def test_action(self):
-        # the harness doesn't (yet!) help much with actions themselves
-        action_event = Mock(params={"fail": ""})
-        self.harness.charm._on_fortune_action(action_event)
+    harness.charm.on.config_changed.emit()
+    config_changed.assert_called_once()
+    config_changed.reset_mock()
 
-        self.assertTrue(action_event.set_results.called)
+    harness.charm.on.remove.emit()
+    remove.assert_called_once()
+    remove.reset_mock()
 
-    def test_action_fail(self):
-        action_event = Mock(params={"fail": "fail this"})
-        self.harness.charm._on_fortune_action(action_event)
 
-        self.assertEqual(action_event.fail.call_args, [("fail this",)])
+def test_on_install_active(harness, mocked_lightkube_client, mocker):
+    mocked_apply = mocker.patch(
+        "k8s_resource_handler.kubernetes._kubernetes_resource_handler.KubernetesResourceHandler.apply"
+    )
+    mocked_apply.return_value = None
 
-    def test_httpbin_pebble_ready(self):
-        # Check the initial Pebble plan is empty
-        initial_plan = self.harness.get_container_pebble_plan("httpbin")
-        self.assertEqual(initial_plan.to_yaml(), "{}\n")
-        # Expected plan after Pebble ready with default config
-        expected_plan = {
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": "🎁"},
-                }
-            },
-        }
-        # Get the httpbin container from the model
-        container = self.harness.model.unit.get_container("httpbin")
-        # Emit the PebbleReadyEvent carrying the httpbin container
-        self.harness.charm.on.httpbin_pebble_ready.emit(container)
-        # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan("httpbin").to_dict()
-        # Check we've got the plan we expected
-        self.assertEqual(expected_plan, updated_plan)
-        # Check the service was started
-        service = self.harness.model.unit.get_container("httpbin").get_service("httpbin")
-        self.assertTrue(service.is_running())
-        # Ensure we set an ActiveStatus with no message
-        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+    harness.begin()
+    harness.charm.on.install.emit()
+    mocked_apply.assert_called()
+    assert harness.model.unit.status == ActiveStatus()
+
+
+@pytest.mark.parametrize(
+    "apply_error, raised_exception, expected_blocked_message",
+    (
+        (FakeApiError(400), pytest.raises(FakeApiError), "ApiError: 400"),
+        (
+            FakeApiError(403),
+            pytest.raises(ReconcileError),
+            "ReconcileError: Failed to reconcile charm resources",
+        ),
+    ),
+)
+def test_on_install_blocked(
+    apply_error,
+    raised_exception,
+    expected_blocked_message,
+    harness,
+    mocked_lightkube_client,
+    mocker,
+):
+    mocked_apply = mocker.patch(
+        "k8s_resource_handler.kubernetes._kubernetes_resource_handler.KubernetesResourceHandler.apply"
+    )
+    mocked_apply.side_effect = apply_error
+
+    harness.begin()
+    with raised_exception:
+        harness.charm.on.install.emit()
+    assert harness.model.unit.status == BlockedStatus(expected_blocked_message)
