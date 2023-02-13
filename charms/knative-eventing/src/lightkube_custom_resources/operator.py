6e16f144ea196c47cffe596327c35ddd48a2abfa
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-02-13 16:59:49 -0600

    feat: bump api version for CRD alpha->beta

--- charms/knative-eventing/src/lightkube_custom_resources/operator.py
+++ charms/knative-eventing/src/lightkube_custom_resources/operator.py
@@ -6,10 +6,9 @@
 from lightkube.generic_resource import create_namespaced_resource
 
 # Knative Operator's KnativeEventing CRDs
-# version 1.1.x
-KnativeEventing_v1alpha1 = create_namespaced_resource(
+KnativeEventing_v1beta1 = create_namespaced_resource(
     group="operator.knative.dev",
-    version="v1alpha1",
+    version="v1beta1",
     kind="KnativeEventing",
     plural="knativeeventings",
     verbs=None,
