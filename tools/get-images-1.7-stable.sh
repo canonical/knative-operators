f78a2808a77e3f6703decb7ba1e48dd9e9ec27b2
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 09:56:43 -0400

    feat: added sort

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -8,7 +8,7 @@ STATIC_IMAGE_LIST=(
 # dynamic list
 git checkout origin/track/1.8
 IMAGE_LIST=()
-IMAGE_LIST+=($(grep image charms/knative-operator/src/manifests/observability/collector.yaml.j2 | awk '{print $2}'))
+IMAGE_LIST+=($(grep image charms/knative-operator/src/manifests/observability/collector.yaml.j2 | awk '{print $2}' | sort --unique))
 
 printf "%s\n" "${STATIC_IMAGE_LIST[@]}"
 printf "%s\n" "${IMAGE_LIST[@]}"
