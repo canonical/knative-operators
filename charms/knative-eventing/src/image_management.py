ba30ceba35ac08e0a2fc873fbfc55683956a9404
Andrew Scribner <ca.scribner+1@gmail.com>
2023-07-28 13:02:22 -0400

    add juju config to use custom images for knative eventing (#129)
    * feat: add juju config to use custom images for knative eventing
    
    This adds:
    
    * the `custom_images` config to to the knative eventing charm, allowing users to provide overrides to the default images so they can use their own (for example, if they work in an air-gapped environment)
    * `DEFAULT_IMAGES` defined in the charm that allow the charm to have an opinion on what images should be used if none are specified
    * tests for the above features
    
    ### Manual test instructions
    ```
    cd charms/knative-serving
    charmcraft pack
    juju deploy knative-operator --trust --channel latest/edge
    juju deploy ./knative-eventing_ubuntu-20.04-amd64.charm --trust --config namespace=knative-eventing
    kubectl get pod -n knative-eventing  # observe the knative eventing pods
    juju config knative-eventing custom_images="{eventing-controller: not-a-real-image}"
    kubectl get pod -n knative-eventing  # observe how a new eventing-controller pod is started.  This will not deploy successfully as the image above is not real
    juju config knative-eventing custom_images=""  # reset back to defaults
    kubectl get pod -n knative-eventing  # observe the knative eventing-controller pods
    ```

---
+++ charms/knative-eventing/src/image_management.py
@@ -0,0 +1,35 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+from typing import Dict
+
+import yaml
+
+
+def parse_image_config(image_config: str) -> Dict[str, str]:
+    """Parses config data for a dict of images, returning the parsed value as a dict.
+
+    This helper does not catch YAML parsing errors, leaving that up to the calling function.
+    Generally, this should be called inside a `try/except YAMLError` block.
+    """
+    images_from_config = yaml.safe_load(image_config)
+
+    if not images_from_config:
+        images_from_config = {}
+
+    images_from_config = remove_empty_images(images=images_from_config)
+
+    return images_from_config
+
+
+def remove_empty_images(images: Dict[str, str]):
+    """Removes any image with a value of an empty string, returning a new dict of the images."""
+    return {name: value for name, value in images.items() if value != ""}
+
+
+def update_images(default_images: Dict[str, str], custom_images: Dict[str, str]) -> Dict[str, str]:
+    """Returns a copy of default_images that is updated with overrides from custom_images."""
+    images = default_images.copy()
+    images.update(custom_images)
+    return images
