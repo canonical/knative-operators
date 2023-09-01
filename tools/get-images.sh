35bd5a1d5da498711c01c48095c636379d1011d3
Ivan Chvets <ivan.chvets@canonical.com>
2023-09-01 10:35:23 -0400

    fix: remove nulls from lists and fixed typo

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -13,7 +13,7 @@ IMAGE_LIST=()
 IMAGE_LIST+=($(find . -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
 IMAGE_LIST+=($(grep image charms/knative-operator/src/manifests/observability/collector.yaml.j2 | awk '{print $2}' | sort --unique))
 # obtain knative eventing version and corresponding knative release information
-KNATIVE_EVENTING_VERSION=$(yq '.options.version.default' ./charms/knative-eventing/config.yaml)
+KNATIVE_EVENTING_VERSION=$(yq -N '.options.version.default' ./charms/knative-eventing/config.yaml)
 KNATIVE_EVENTING_REPO_DOWNLOAD_URL=https://github.com/knative/eventing/releases/download/
 EVENTING_IMAGE_LIST=()
 wget -q "${KNATIVE_EVENTING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_EVENTING_VERSION}/eventing-core.yaml"
@@ -21,7 +21,7 @@ EVENTING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./
 wget -q "${KNATIVE_EVENTING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_EVENTING_VERSION}/eventing.yaml"
 EVENTING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./eventing.yaml))
 # obtain knative serving version and corresponding knative release information
-KNATIVE_SERVING_VERSION=$(yq '.options.version.default' ./charms/knative-serving/config.yaml)
+KNATIVE_SERVING_VERSION=$(yq -N '.options.version.default' ./charms/knative-serving/config.yaml)
 KNATIVE_SERVING_REPO_DOWNLOAD_URL=https://github.com/knative/serving/releases/download/
 SERVING_IMAGE_LIST=()
 wget -q "${KNATIVE_SERVING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_SERVING_VERSION}/serving-core.yaml"
@@ -37,17 +37,23 @@ SERVING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./s
 wget -q "${KNATIVE_SERVING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_SERVING_VERSION}/serving-post-install-jobs.yaml"
 SERVING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./serving-post-install-jobs.yaml))
 # obtain net istio images based on hardcoded version
-NET_ISTIO_VERSION=${KNATIVE_SERVING_VERSION}
-
+NET_ISTIO_VERSION="${KNATIVE_SERVING_VERSION}"
 # NOTE: for KNative Serving version 1.10.2 Net Istio version must be set to 1.11.0
 #       this need to be reviewed if KNative Serving version is changed
-if [ KNATIVE_SERVING_VERSION == "1.10.2" ]; then
+if [ $KNATIVE_SERVING_VERSION == "1.10.2" ]; then
   NET_ISTIO_VERSION="1.11.0"
 fi
 NET_ISTIO_REPO_DOWNLOAD_URL=https://github.com/knative-extensions/net-istio/releases/download/
 NET_ISTIO_IMAGE_LIST=()
 wget -q "${NET_ISTIO_REPO_DOWNLOAD_URL}knative-v${NET_ISTIO_VERSION}/net-istio.yaml"
 NET_ISTIO_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./net-istio.yaml))
+
+# remove nulls from arrays
+del_null="null"
+EVENTING_IMAGE_LIST=("${EVENTING_IMAGE_LIST[@]/$del_null}")
+SERVING_IMAGE_LIST=("${SERVING_IMAGE_LIST[@]/$del_null}")
+NET_ISTIO_IMAGE_LIST=("${NET_ISTIO_IMAGE_LIST[@]/$del_null}")
+IMAGE_LIST=("${IMAGE_LIST[@]/$del_null}")
 
 # TO-DO not printing static list
 #printf "%s\n" "${STATIC_IMAGE_LIST[@]}" | sort -u
