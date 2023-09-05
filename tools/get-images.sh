a22dd6ec77aa1628db0c4369731c2f6c485e25c9
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-09-05 03:48:42 -0400

    feat: get images on main (#141)
    Add a script that produces list of container images managed by charms on this repository and/or by corresponding workloads.
    Refs canonical/bundle-kubeflow#679
    Closes #142 
    
    Co-authored-by: Orfeas Kourkakis <orfeas.kourkakis@canonical.com>

---
+++ tools/get-images.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# This script returns list of container images that are managed by this charm and/or its workload
+
+IMAGE_LIST=()
+IMAGE_LIST+=($(find . -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
+IMAGE_LIST+=($(grep image charms/knative-operator/src/manifests/observability/collector.yaml.j2 | awk '{print $2}' | sort --unique))
+# obtain knative eventing version and corresponding knative release information
+KNATIVE_EVENTING_VERSION=$(yq -N '.options.version.default' ./charms/knative-eventing/config.yaml)
+KNATIVE_EVENTING_REPO_DOWNLOAD_URL=https://github.com/knative/eventing/releases/download/
+EVENTING_IMAGE_LIST=()
+wget -q "${KNATIVE_EVENTING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_EVENTING_VERSION}/eventing-core.yaml"
+EVENTING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./eventing-core.yaml))
+wget -q "${KNATIVE_EVENTING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_EVENTING_VERSION}/eventing.yaml"
+EVENTING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./eventing.yaml))
+wget -q "${KNATIVE_EVENTING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_EVENTING_VERSION}/eventing-post-install.yaml"
+EVENTING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./eventing-post-install.yaml))
+# obtain knative serving version and corresponding knative release information
+KNATIVE_SERVING_VERSION=$(yq -N '.options.version.default' ./charms/knative-serving/config.yaml)
+KNATIVE_SERVING_REPO_DOWNLOAD_URL=https://github.com/knative/serving/releases/download/
+SERVING_IMAGE_LIST=()
+wget -q "${KNATIVE_SERVING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_SERVING_VERSION}/serving-core.yaml"
+SERVING_IMAGE_LIST+=($(yq -N '.spec.image' ./serving-core.yaml))
+SERVING_IMAGE_LIST+=($(yq -N '.data.queue-sidecar-image' ./serving-core.yaml))
+SERVING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./serving-core.yaml))
+wget -q "${KNATIVE_SERVING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_SERVING_VERSION}/serving-hpa.yaml"
+SERVING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./serving-hpa.yaml))
+wget -q "${KNATIVE_SERVING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_SERVING_VERSION}/serving-default-domain.yaml"
+SERVING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./serving-default-domain.yaml))
+wget -q "${KNATIVE_SERVING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_SERVING_VERSION}/serving-storage-version-migration.yaml"
+SERVING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./serving-storage-version-migration.yaml))
+wget -q "${KNATIVE_SERVING_REPO_DOWNLOAD_URL}/knative-v${KNATIVE_SERVING_VERSION}/serving-post-install-jobs.yaml"
+SERVING_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./serving-post-install-jobs.yaml))
+# obtain net istio images based on hardcoded version
+NET_ISTIO_VERSION="${KNATIVE_SERVING_VERSION}"
+# NOTE: for KNative Serving version 1.10.2 Net Istio version must be set to 1.11.0
+#       this need to be reviewed if KNative Serving version is changed
+if [ $KNATIVE_SERVING_VERSION == "1.10.2" ]; then
+  NET_ISTIO_VERSION="1.11.0"
+fi
+NET_ISTIO_REPO_DOWNLOAD_URL=https://github.com/knative-extensions/net-istio/releases/download/
+NET_ISTIO_IMAGE_LIST=()
+wget -q "${NET_ISTIO_REPO_DOWNLOAD_URL}knative-v${NET_ISTIO_VERSION}/net-istio.yaml"
+NET_ISTIO_IMAGE_LIST+=($(yq -N '.spec.template.spec.containers | .[] | .image' ./net-istio.yaml))
+
+# remove nulls from arrays
+del_null="null"
+EVENTING_IMAGE_LIST=("${EVENTING_IMAGE_LIST[@]/$del_null}")
+SERVING_IMAGE_LIST=("${SERVING_IMAGE_LIST[@]/$del_null}")
+NET_ISTIO_IMAGE_LIST=("${NET_ISTIO_IMAGE_LIST[@]/$del_null}")
+IMAGE_LIST=("${IMAGE_LIST[@]/$del_null}")
+
+printf "%s\n" "${EVENTING_IMAGE_LIST[@]}" | sed -r '/^\s*$/d' | sort -u
+printf "%s\n" "${SERVING_IMAGE_LIST[@]}" | sed -r '/^\s*$/d' | sort -u
+printf "%s\n" "${NET_ISTIO_IMAGE_LIST[@]}" | sed -r '/^\s*$/d' | sort -u
+printf "%s\n" "${IMAGE_LIST[@]}" | sed -r '/^\s*$/d' | sort -u
+
+rm eventing-core.yaml eventing-post-install.yaml eventing.yaml net-istio.yaml \
+  serving-core.yaml serving-default-domain.yaml serving-hpa.yaml \
+  serving-post-install-jobs.yaml serving-storage-version-migration.yaml
