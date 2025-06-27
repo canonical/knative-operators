dba22c6170059f8a382687fb51fbaaf51a6d334d
Noha Ihab <49988746+NohaIhab@users.noreply.github.com>
2025-06-27 11:38:27 +0300

    fix: delete redundant namespace yaml and add integration test (#339)
    Closes #337 
    
    * Removes the namespace manifest yaml from `knative-operator` charm, 
    there is no need for this since the resources can be created in the same 
    namespace as the rest of the charms in the bundle (`kubeflow` namespace).
    * Adds an integration test for scaling down the `knative-operator` charm as a 
    safety measure that the deletion does not affect the namespace where the
    charm is deployed.

--- charms/knative-operator/tests/integration/test_charm.py
+++ charms/knative-operator/tests/integration/test_charm.py
@@ -13,7 +13,10 @@ from charmed_kubeflow_chisme.testing import (
     deploy_and_assert_grafana_agent,
     get_alert_rules,
 )
+from lightkube import Client
+from lightkube.resources.core_v1 import Namespace
 from pytest_operator.plugin import OpsTest
+from tenacity import Retrying, stop_after_delay, wait_fixed
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 CHARM_ROOT = "."
@@ -80,3 +83,50 @@ async def test_alert_rules(ops_test):
     alert_rules = get_alert_rules()
     log.info("found alert_rules: %s", alert_rules)
     await assert_alert_rules(app, alert_rules)
+
+
+RETRY_FOR_TWO_MINUTES = Retrying(
+    stop=stop_after_delay(60 * 2),
+    wait=wait_fixed(5),
+    reraise=True,
+)
+
+
+async def test_scale_down_app_namespace_unaffected(ops_test):
+    """
+    Test scaling down the application to 0 does not affect the namespace.
+    """
+
+    # Scale down to 0
+    await ops_test.model.applications["knative-operator"].scale(scale=0)
+
+    # Wait until scale down takes effect
+    for attempt in RETRY_FOR_TWO_MINUTES:
+        with attempt:
+
+            # Get application status
+            model_status = await ops_test.model.get_status()
+            app_status = model_status.applications["knative-operator"].status["status"]
+
+            # Get number of units
+            num_units = len(model_status.applications["knative-operator"].units)
+
+            log.info(
+                f"Waiting for application to scale down, status is {app_status}, Number of units is {num_units}"
+            )
+
+            # Assert the status is unknown and number of units is 0
+            # This is the expected state when the application is scaled down to 0
+            assert app_status == "unknown"
+            assert num_units == 0
+
+    client = Client()
+
+    # Get the status of the test namespace
+    namespace = client.get(res=Namespace, name=ops_test.model_name)
+    namespace_status = namespace.status.phase
+
+    # Assert namespace is Active
+    assert (
+        namespace_status == "Active"
+    ), f"Expected namespace to be 'Active', but got '{namespace_status}'"
